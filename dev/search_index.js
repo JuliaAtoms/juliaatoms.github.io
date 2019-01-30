b24ac3e955a46c5afe0e155b214914c4ec8d57f9
zeptodoctor <44736852+zeptodoctor@users.noreply.github.com>
2019-01-30 22:47:00 +0000

    build based on 140cac0

---
+++ dev/search_index.js
@@ -0,0 +1,19 @@
+var documenterSearchIndex = {"docs": [
+
+{
+    "location": "#",
+    "page": "Home",
+    "title": "Home",
+    "category": "page",
+    "text": ""
+},
+
+{
+    "location": "#JuliaAtoms-1",
+    "page": "Home",
+    "title": "JuliaAtoms",
+    "category": "section",
+    "text": "This is the landing page of the JuliaAtoms GitHub organisation."
+},
+
+]}
